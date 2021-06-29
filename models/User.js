cc54ef5f229e5a2ee697491293aa49bd242aae41
Shay Wagner <srwagner916@gmail.com>
2021-06-29 18:10:07 -0500

    wrote user model

---
+++ models/User.js
@@ -0,0 +1,45 @@
+const { model, Schema } = require('mongoose');
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+      }
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false
+  }
+);
+
+// gets the total number of friends
+UserSchema.virtual('friendCount').get(function() {
+  return this.friends.length + 1;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
