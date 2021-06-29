100fa2713cbfb9043da4e6293e281848166580c2
Shay Wagner <srwagner916@gmail.com>
2021-06-29 18:41:08 -0500

    wrote schemas for thoughts and reactions. need to write util to format date and virtual to get reaction count

---
+++ models/Thought.js
@@ -0,0 +1,64 @@
+const { Schema, model, Types } = require('mongoose');
+
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    reationBody: {
+      type: String,
+      required: true,
+      maxLength: 280
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      // format date
+      //get:
+    }
+  },
+  {
+    toJSON: {
+      getters: true
+    },
+    id: false
+  }
+);
+
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLenght: 1,
+      maxLength: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+      // Format the date!
+      // get:
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    reactions: [reactionSchema]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
